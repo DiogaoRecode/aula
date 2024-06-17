443f9af3fa8da6bac0b51263498b673e9f3f8f6f
Diogo Ramalho <diogoeng.mobilidade@gmail.com>
2024-06-17 20:13:04 -0300

    Aula 3 - atividade em Squad

---
+++ Aula 3/aula3Squad.py
@@ -0,0 +1,7 @@
+print("Por favor informe os valores das 4 notas abaixo:")
+nota1 = float(input("Digite a nota da primeira prova:"))
+nota2 = float(input("Digite a nota da segunda prova:"))
+nota3 = float(input("Digite a nota da terceira prova:"))
+nota4 = float(input("Digite a nota da quarta prova:"))
+media= (nota1+nota2+nota3+nota4)/4
+print(f"O valor da média das suas provas foi {media:.2f}")
